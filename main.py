d2c4e7bdd73399796e344f1a0aa74b008c049082
trentmcomber <mcombertrent@gmail.com>
2025-07-20 12:43:45 -0600

    Read File frankenstein

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+def get_book_text(path_to_file):
+    with open(path_to_file) as f:
+        file_contents = f.read()
+        return file_contents
+    
+def main():
+    full_text = get_book_text("books/frankenstein.txt")
+    print(full_text)
+
+
+main()
+
 
